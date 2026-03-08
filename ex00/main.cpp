d8ecbb678cb83e8086cdbf095ed58e7968131514
hladeiro <hugoladeiro@proton.me>
2026-03-08 23:21:15 +0000

    adding a new error for pipeline purposes

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -25,6 +25,8 @@ int main(void)
 
 	Bureaucrat* bo = new Bureaucrat("Bob", 2);
 
+	std::cout << bo;
+
 	std::cout << "\033[33m" << std::endl << "Test increasing" << "\033[0m" << std::endl;
 	Bureaucrat bob("Bob", 2);
 	std::cout << bob;
